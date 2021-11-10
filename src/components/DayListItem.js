9fbc9a0e95c29dc4dbc1a8baf592f51a768f5e4e
noahthomlison <noahthomlison@gmail.com>
2021-11-09 20:36:59 -0800

    adds a function to correct messaging for 1 or 0 days left'

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,12 +6,26 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
 
    let dayClass = classNames("day-list__item", {'day-list__item--selected': props.selected}, {'day-list__item--full': !props.spots});
+   let spotsMessage = 2
 
+   const formatSpots = () => {
+      if (props.spots ===  1){
+         spotsMessage = 1
+       }
+
+      if (props.spots === 0){
+         spotsMessage = 0
+      }
+   } 
+
+   formatSpots()
 
    return (
      <li onClick={() => props.setDay(props.name)} className={dayClass}>
-       <h2 className="text--regular">{props.name}</h2> 
-       <h3 className="text--light">{props.spots} spots remaining</h3>
+      {spotsMessage === 2 && <h2 className="text--regular">{props.spots} spots remaining</h2>}
+      {spotsMessage === 1 && <h2 className="text--regular">{props.spots} spot remaining</h2>}
+      {spotsMessage === 0 && <h2 className="text--regular">no spots remaining</h2>}
+      <h3 className="text--light">{props.spots}</h3>
      </li>
    );
  }
