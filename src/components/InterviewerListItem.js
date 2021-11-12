2e8c67629978dfde81135baff8a45689810d0061
noahthomlison <noahthomlison@gmail.com>
2021-11-12 09:19:34 -0800

    corrects function name, corrects props.id

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,11 +3,11 @@ import classNames from "classnames";
 
 import "components/InterviewerListItem.scss"
 
-export default function DayListItem(props) {
+export default function InterviewerListItem(props) {
   let interviewerClass = classNames("interviewers__item", {'interviewers__item--selected': props.selected});
 
    return (
-    <li onClick={() => props.setInterviewer(props.name)} className={interviewerClass}>
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
