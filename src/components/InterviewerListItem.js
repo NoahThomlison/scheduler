d1f32782db8218b65ecd37f2ffc1021069d138cc
noahthomlison <noahthomlison@gmail.com>
2021-11-12 07:49:54 -0800

    updates code to add dymanic props values and onclick function

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,41 +1,19 @@
 import React from "react";
 import classNames from "classnames";
 
-import "components/InterviewerListItem;
+import "components/InterviewerListItem.scss"
 
 export default function DayListItem(props) {
-
-  const interviewer = {
-    id: 1,
-    name: "Sylvia Palmer",
-    avatar: "https://i.imgur.com/LpaY82x.png"
-  };
-  
-
-  //  let dayClass = classNames("day-list__item", {'day-list__item--selected': props.selected}, {'day-list__item--full': !props.spots});
-  //  let spotsMessage = 2
-
-  //  const formatSpots = () => {
-  //     if (props.spots ===  1){
-  //        spotsMessage = 1
-  //      }
-
-  //     if (props.spots === 0){
-  //        spotsMessage = 0
-  //     }
-  //  } 
-
-  //  formatSpots()
+  let interviewerClass = classNames("interviewers__item", {'interviewers__item--selected': props.selected});
 
    return (
-
-<li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
+    <li onClick={() => props.setInterviewer(props.name)} className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt="Sylvia Palmer"
+      />
+      {props.name}
+    </li>
    );
  }
