7a836edd592c6a60165d7426292a841012a90c42
noahthomlison <noahthomlison@gmail.com>
2021-11-11 17:14:15 -0800

    fixes error

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from "react";
 import DayListItem from "../components/DayListItem";
 
 export default function DayList(props) {
-   
+
    return (
       <ul>
          {props.days.map(function(element, index){
