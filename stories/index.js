e54a6b3c8804a169b8a1e31c189a0d76fcbd3423
noahthomlison <noahthomlison@gmail.com>
2021-11-13 09:32:37 -0800

    adds stories for confirm component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,5 +140,5 @@ storiesOf("Button", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
-  .add("Show", () => <Show onEdit={action("onEdit")}/>)
-  .add("Show", () => <Show onDelete={action("onDelete")}/>)
+  .add("Show", () => <Show onDelete={action("onDelete")} onEdit={action("onEdit")}/>)
+  .add("Confirm", () => <Confirm message="Delete the appointment?" onCancel={action("onCancel")} onConfirm={action("onConfirm")}/>)
