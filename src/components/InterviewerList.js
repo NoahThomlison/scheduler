875122042712b24ab5507d667af12d03338dbbfe
noahthomlison <noahthomlison@gmail.com>
2021-11-13 15:41:52 -0800

    removes a console.log and an unsued event

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,14 +3,14 @@ import "components/InterviewerList.scss"
 import InterviewerListItem from "../components/InterviewerListItem";
 
 export default function InterviewerList(props) {
-  
+  console.log(props)
   const interviewer =  props.interviewers.map(function(interviewer){
     return <InterviewerListItem 
      key={interviewer.id}
      name={interviewer.name} 
      avatar={interviewer.avatar} 
      selected={interviewer.id === props.value}
-     setInterviewer={(event) => props.onChange(interviewer.id)} 
+     setInterviewer={() => props.onChange(interviewer.id)} 
      />
 })
 
