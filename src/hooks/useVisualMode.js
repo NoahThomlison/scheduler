a717b1d4abf2acb343906ccb771808ceb155e001
noahthomlison <noahthomlison@gmail.com>
2021-11-20 16:20:04 -0800

    adds usevisualmode and related testing

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export default function useVisualMode (initial) {
+  const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial]);
+
+  function transition (newMode, replace = false) {
+    setMode(newMode)
+
+    //if replace true then replace last value in history with new value
+    if(replace){
+      setHistory((prevState) => {
+        return(prevState.slice(0, prevState.length-1))
+      })
+    }
+
+    //set history to new array with newMode added
+    setHistory((prevState) => {
+      return([...prevState, newMode])
+    })
+  }
+
+  function back () {
+    //check to confirm that back does not go to far back
+    if(history.length === 1){return}
+
+    //set mode to second last place in history
+    setMode(history[history.length-2])
+
+    //set history to previous value -1 value
+    setHistory((prevState) => {
+      return(prevState.slice(0, prevState.length-1))
+    })
+  }
+
+  console.log(mode)
+  console.log(history)
+
+  return { mode, transition, back }
+}
+
+
