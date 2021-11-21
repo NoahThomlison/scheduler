a9b7e0ebfa411ab1d6baa111a8378b590136d30b
noahthomlison <noahthomlison@gmail.com>
2021-11-21 13:33:41 -0800

    adds new helper function which returns daily interviewers, calls new function, passes daily interviewers down as props to the appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import { useState, useEffect } from "react";
 import  Appointment from "../components/Appointment/"
-import {getAppointmentsForDay, getInterview} from "../helpers/selectors"
+import {getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors"
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -31,6 +31,8 @@ export default function Application(props) {
   const setDay = (day) => setState({ ...state, day });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
+  
   let appointmentsArray = (dailyAppointments).map((appointment) => {
     const interview = getInterview(state, appointment.interview);
     return (
@@ -39,6 +41,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={interviewers}
       />
     );
   });
