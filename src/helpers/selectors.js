f3189f6ff44bdc8bbaf9cd86ff18656341364496
noahthomlison <noahthomlison@gmail.com>
2021-11-21 13:34:27 -0800

    adds new function getInterviewrsForDay which takes in data and a specific day and returns an array of interviewers for that day, or null if none

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -17,6 +17,26 @@ export function getAppointmentsForDay (state, day) {
   return(appointments)
 }
 
+export function getInterviewersForDay (state, day) {
+  //empty array to hold results
+  let interviewers = []
+
+  // filter the data to matching state.name with day given
+  const filteredState = state.days.filter(state => state.name === day)
+  console.log(filteredState)
+
+  //if filtered data is empty, return
+  if(filteredState.length < 1){return(interviewers)
+  }
+  
+  // //loop through filtered data and if the appointmentID matches a day push it to the appointments array
+  filteredState[0].interviewers.forEach((interviewer) => {
+    if(state.interviewers[interviewer]){interviewers.push(state.interviewers[interviewer])}
+  })
+
+  return(interviewers)
+}
+
 //function which returns interview object if an interview is scheduled
 export function getInterview (state, interview) {
 
