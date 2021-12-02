47dae18d384005c7d6d4689ab5ff5095c16c3381
noahthomlison <noahthomlison@gmail.com>
2021-12-01 21:04:37 -0800

    refactors to add a warning if user attempts to make an appointment without entering name

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,8 @@ export default function Form (props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
   const reset = () => {
     setStudent("")
     setInterviewer(null)
@@ -16,11 +18,21 @@ export default function Form (props) {
     props.onCancel()
   }
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off"  onSubmit={event => event.preventDefault()}>
           <input
+            data-testid="student-name-input"
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
@@ -29,6 +41,7 @@ export default function Form (props) {
             onChange={(event) => setStudent(event.target.value)}
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           value={interviewer}
           interviewers={props.interviewers}
@@ -38,7 +51,8 @@ export default function Form (props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={()=>cancel()}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)} >Save</Button>
+          <Button confirm onClick={() => validate()} >Save</Button>
+
         </section>
       </section>
     </main>
